130f2c159a96c93bbf6e94479bdb0589d395ee4f
Istvan <olivaistvan@gmail.com>
2022-01-25 21:21:23 -0300

    Requisito 04 - Criar Links de navegacao

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import Search from '../pages/Search';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -25,6 +27,9 @@ export default class Header extends Component {
       <header data-testid="header-component">
         <h1>Trybetunes</h1>
         <h3 data-testid="header-user-name">{ !userName ? <Loading /> : userName }</h3>
+        <Link to="/search" data-testid="link-to-search">Pesquisar</Link>
+        <Link to="/favorites" data-testid="link-to-favorites">Favoritos</Link>
+        <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
       </header>
     );
   }
